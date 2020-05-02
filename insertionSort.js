a71395f277cd145dd78b03a184f847eea2919387
SR Sajjad <sajjadsr001@gmail.com>
2020-05-02 10:27:36 +0600

    Create insertionSort.js

---
+++ insertionSort.js
@@ -0,0 +1,21 @@
+function insertionSort(arr) {
+  // sorting
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j >= 0; j--) {
+      let curr = arr[j];
+      let prev = arr[j - 1];
+
+      if (!isFinite(prev)) break;
+
+      if (curr > prev) break;
+      
+      else {
+        //swap
+        arr[j] = prev;
+        arr[j - 1] = curr;
+      }
+    }
+  }
+
+  return arr;
+}
